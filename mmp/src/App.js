c9c001d5a5766c778303c68c8bf3e839ae82ecbc
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2024-08-18 22:06:10 +0530

    update Loader.jsx

---
+++ mmp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Loader } from './Loader/Loader';
+
+function App() {
+  return (
+    <div className="App">
+      <Loader/>
+    </div>
+  );
+}
+
+export default App;
